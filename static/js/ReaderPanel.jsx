919564feef85b42ae5ecd9610affa6d026dbef3c
rayistern <92346730+rayistern@users.noreply.github.com>
2023-04-30 13:25:04 -0400

    MORE FIXES

--- static/js/ReaderPanel.jsx
+++ static/js/ReaderPanel.jsx
@@ -1602,7 +1602,7 @@ class ReaderDisplayOptionsMenu extends Component {
       {name: "aliyotOn",   content: Sefaria._("On"), role: "radio", ariaLabel: Sefaria._("Show Parasha Aliyot") },
       {name: "aliyotOff", content: Sefaria._("Off"), role: "radio", ariaLabel: Sefaria._("Hide Parasha Aliyot") },
     ];
-    let aliyahToggle = this.renderAliyotToggle() ? (
+    {/*let aliyahToggle = this.renderAliyotToggle() ? (
       this.props.parentPanel == "Sheet" ? null :
         <ToggleSet
           ariaLabel="Toggle Aliyot"
@@ -1611,7 +1611,7 @@ class ReaderDisplayOptionsMenu extends Component {
           options={aliyahOptions}
           setOption={this.props.setOption}
           currentValue={this.props.settings.aliyotTorah} />) : null;
-
+    */}
     let vowelsOptions = [
       {name: "all", content: "<span class='he'>אָ֑</span>", role: "radio", ariaLabel: Sefaria._("Show Vowels and Cantillation")},
       {name: "partial", content: "<span class='he'>אָ</span>", role: "radio", ariaLabel: Sefaria._("Show only vowel points")},
@@ -1665,7 +1665,7 @@ class ReaderDisplayOptionsMenu extends Component {
                   {layoutToggle}
                   {colorToggle}
                   {sizeToggle}
-                  {aliyahToggle}
+                  {/*{aliyahToggle}*/}
                   {vowelToggle}
                   {punctuationToggle}
                 </div>
